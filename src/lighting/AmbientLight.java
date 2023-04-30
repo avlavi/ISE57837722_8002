5afb648f0f8d8cf4cd657118ff26f16e6504136a
ShiloSofer <shilosouffir@gmail.com>
2023-04-30 18:14:09 +0300

    PRO5.1

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -1,9 +1,13 @@
 package lighting;
 
+import geometries.Geometries;
 import primitives.Color;
 import primitives.Double3;
 
 public class AmbientLight {
     private Color lA;
+    private Color intensity;
     private Double3 kA;
+    AmbientLight ambientLight; //= AmbientLight.NONE;
+    Geometries geometries = new Geometries();
 }
