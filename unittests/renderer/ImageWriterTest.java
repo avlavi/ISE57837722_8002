36f28b55918040fb97509b9cb947e39573523fc1
avlavi <2017aviad@gmail.com>
2023-05-01 12:24:35 +0300

    PRO5.1

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,9 +18,26 @@ class ImageWriterTest {
     @Test
     void testWriteToImage() {
         // ============ Equivalence Partitions Tests ==============
+        ImageWriter imageWriter = new ImageWriter("test", 800, 500);
+        for (int i = 0; i < 500; ) {
+            for (int j = 0; j < 800; j++) {
+                imageWriter.writePixel(j, i, new Color(255, 0, 0));
+            }//line of rad
+            i++;
+            for (int h = 0; h < 49; h++, i++) {
+                for (int j = 0; j < 800; j += 50) {//column of red
+                    imageWriter.writePixel(j, i, new Color(255, 0, 0));
+                    for (int k = 1; k < 50; k++) {//column of yellow
+                        imageWriter.writePixel(k+j, i, new Color(255, 255, 0));
+                    }
+                }
 
+            }
 
-        // =============== Boundary Values Tests ==================
+        }
+        imageWriter.writeToImage();
 
     }
+    // =============== Boundary Values Tests ==================
+
 }
